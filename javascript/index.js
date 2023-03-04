bb72f03e88179c98102331ee122e16e0f744a0e7
Rae40 <103543425+Rae40@users.noreply.github.com>
2023-03-04 17:26:14 -0500

    Added the current location

--- javascript/index.js
+++ javascript/index.js
@@ -9,7 +9,7 @@ losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM	Do YYYY");
 losAngelesTimeElement.innerHTML = losAngelesTime.format(
   "h:mm:ss [<small>]A[</small>]"
 );
-}
+
 
 
 
@@ -23,11 +23,15 @@ let sydneyTime = moment().tz("Australia/sydney");
 sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
 sydneyTimeElement.innerHTML = sydneyTime.format(
   "h:mm:ss [<small>] A [</small>]"
-);
+)
+};
 
 
 function updateCity(event){
   let cityTimeZone = event.target.value;
+  if(cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_" , " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
